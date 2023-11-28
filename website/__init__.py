c70718cacc9134776582a814d53e30b32b7063d3
ch1llc0d3 <128004915+ch1llc0d3@users.noreply.github.com>
2023-11-28 06:48:20 -0300

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,6 +13,8 @@ def create_app():
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    bcrypt = Bcrypt(app)
+
 
     from .views import views
     from .auth import auth
